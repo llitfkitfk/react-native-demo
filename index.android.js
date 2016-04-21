f6298fba70f5a4763cf13b7c05935349460f8963
paul <paul@wizmacau.com>
2016-04-21 18:48:56 +0800

    fixed android app name

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
 var React = require('react-native');
 var { AppRegistry } = React;
 var DemoApp = require('./src/listview/BookList');
-AppRegistry.registerComponent('Touch', () => DemoApp);
+AppRegistry.registerComponent('DemoApp', () => DemoApp);
